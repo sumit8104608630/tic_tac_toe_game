5532693d61a78514cb1380aadd097ee72bb2d5ab
sss/js dev <114969811+sumit8104608630@users.noreply.github.com>
2025-01-23 20:51:03 +0530

    Update toe.js

--- toe.js
+++ toe.js
@@ -57,6 +57,7 @@ console.log(arr)
  }
 //game main engine 
 function checking_winner(){
+ console.log(arr)
  if (arr[0]!==null&&arr[0]==arr[1]&&arr[1]==arr[2]||
      arr[3]!==null&&arr[3]==arr[4]&&arr[4]==arr[5]||
      arr[6]!==null&&arr[6]==arr[7]&&arr[7]==arr[8]||
